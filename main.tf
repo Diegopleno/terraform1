7cf6d6766ad037b50b0f94c7c65db0c789cbe8ce
Diego Gomes <dsgomes.pleno@gmail.com>
2026-03-24 19:10:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0007e082d5009529b"
   instance_type = "t2.micro"
   key_name = "dsgomeslab"
-
+  vpc_security_group_ids = ["sg-0123456789abcdef"]
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo "<h1>Feito com Terraform</h1>" > index.html
+                nohup busybox httpd -f -p 80 &
+                  EOF
   tags = {
     Name = "srv001-terra"
   }
