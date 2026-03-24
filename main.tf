877f4e5d09e3eb75dc3bb7207f618f2f64548a78
Diego Gomes <dsgomes.pleno@gmail.com>
2026-03-24 19:11:18 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ resource "aws_instance" "app_server" {
                 nohup busybox httpd -f -p 80 &
                   EOF
   tags = {
-    Name = "srv001-terra"
+    Name = "srv001-teste"
   }
 }
