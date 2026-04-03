e61bde90bf8481a0a544ba9becbd9aa74978c956
Diego Gomes <dsgomes.pleno@gmail.com>
2026-04-02 22:21:22 -0300

    Alterção no Tf

--- main.tf
+++ main.tf
@@ -18,12 +18,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name = "dsgomeslab"
   vpc_security_group_ids = ["sg-0dbb42b8aeb1adb7d"]
-/*  user_data = <<-EOF
-                #!/bin/bash
-                cd /home/ubuntu
-                echo "<h1>Terraform ansible python</h1>" > index.html
-                nohup busybox httpd -f -p 80 &
-                  EOF*/
   tags = {
     Name = "Terraform Ansible Python"
   }
